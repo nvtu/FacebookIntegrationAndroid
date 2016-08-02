30355106878ffddf65b563dbb984804ef55a009d
Tu Van Ninh <Tu Van Ninh>
2016-08-03 01:17:27 +0700

    First commit Facebook Integration on 03/08/2016

---
+++ Readme.md
@@ -0,0 +1,47 @@
+Facebook Integration with Android Facebook SDK 4.0: 
+
+---Features: 
+
+1) Login Facebook (with Facebook Login Button and Customized Button), check current access
+
+2) Share Photo on Facebook
+
+3) Get user Profile: id, name, email, birthday, profile picture
+
+4) Get user's friend list who use this app
+
+---Preparation:
+
+1) Get facebook debug key hash:
+
+- Download openssl-0.9.8e_X64.zip on https://code.google.com/archive/p/openssl-for-windows/downloads
+
+- Extract the file to folder to C:\OpenSSL
+
+- Find the debug.keystore file path (It is in folder C:\Users\%UserName%\.android)
+
+- At the path: C:\Program Files\Java\jre1.8.0_65\bin, open cmd and paste the following code:
+
+keytool -exportcert -alias androiddebugkey -keystore C:\Users\%UserName%\.android\debug.keystore | C:\OpenSSL\openssl sha1 -binary | C:\OpenSSL\openssl base64
+
+2) Get facebook release key hash:
+
+- Follow the instruction on https://developer.android.com/studio/publish/app-signing.html to get the release key app (.jks file)
+
+- Notice to put an alias name to your release key
+
+- At the path: C:\Program Files\Java\jre1.8.0_65\bin, open cmd and paste the following code:
+
+keytool -exportcert -alias yourappreleasekeyalias -keystore ~/.your/path/release.keystore | C:\OpenSSL\openssl sha1 -binary | C:\OpenSSL\openssl base64
+
+--- Reference and More Information:
+
+https://developers.facebook.com/docs/android/getting-started
+
+https://developers.facebook.com/docs/facebook-login/android
+
+http://stackoverflow.com/questions/13896892/facebook-sdk-3-0-android
+
+http://stackoverflow.com/questions/29295987/android-facebook-4-0-sdk-how-to-get-email-date-of-birth-and-gender-of-user
+
+http://stackoverflow.com/questions/29491479/fetch-friends-list-from-facebook-sdk-4-0-1-in-android-with-graph-api-2-2
---
+++ app/src/main/java/com/example/tuvanninh/facebookintegration/MainActivity.java
@@ -0,0 +1,289 @@
+package com.example.tuvanninh.facebookintegration;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA = 0;
+    private static final int SELECT_FILE = 1;
+    private String email,birthday,name, id;
+    LoginButton loginButton;
+    ImageButton imageButton, shareImageBut;
+    ProfilePictureView userPic;
+    TextView userName, userBD, userEmail;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        setContentView(R.layout.activity_main);
+
+        initComponent();
+        loginWithFacebookButton();
+        loginWithCustomizeButton();
+        setUpSharingPhotoButton();
+
+        if (checkIfAlreadyLogin() == true){
+            getUserProfile();
+            getUserFriendList();
+        }
+
+
+    }
+
+    //Set up button section
+    private void updateLayout() {
+        userName.setText(name);
+        userEmail.setText(email);
+        userBD.setText(birthday);
+        userPic.setProfileId(id);
+
+    }
+
+    private void setUpSharingPhotoButton() {
+        shareImageBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+    }
+
+    private boolean checkIfAlreadyLogin() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    private void initComponent() {
+        userPic = (ProfilePictureView) findViewById(R.id.userPic);
+        userName = (TextView) findViewById(R.id.userName);
+        userEmail = (TextView) findViewById(R.id.userEmail);
+        userBD = (TextView) findViewById(R.id.userBD);
+        imageButton = (ImageButton) findViewById(R.id.loginBut);
+        shareImageBut = (ImageButton) findViewById(R.id.shareImage);
+        loginButton = (LoginButton) findViewById(R.id.login_Button);
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile", "user_friends", "user_birthday"));
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == SELECT_FILE){
+                onSelectFromGalleryResult(data);
+            }
+            else if (requestCode == REQUEST_CAMERA){
+                onCaptureImageResult(data);
+            }
+        }
+    }
+
+    private void loginWithFacebookButton() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                getUserProfile();
+                getUserFriendList();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void loginWithCustomizeButton() {
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.performClick();
+            }
+        });
+    }
+
+    //Sharing photo on Facebook by taking photo and choose from library
+    private void selectImage(){
+        final CharSequence[] items = {"Take photo", "Choose from Library", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Photo");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (items[which].equals("Take photo")){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                }
+                else if (items[which].equals("Choose from Library")){
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+                }
+                else{
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void onSelectFromGalleryResult(Intent data){
+        Uri selectedImageUri = data.getData();
+        String[] projection = {MediaStore.MediaColumns.DATA};
+        Cursor cursor = managedQuery(selectedImageUri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+        String selectedImagePath = cursor.getString(column_index);
+        Bitmap thumbnail;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(selectedImagePath, options);
+        final int REQUIRED_SIZE = 200;
+        int scale = 1;
+        while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+            scale *= 2;
+        options.inSampleSize = scale;
+        options.inJustDecodeBounds = false;
+        thumbnail = BitmapFactory.decodeFile(selectedImagePath, options);
+
+        ShareDialog(thumbnail);
+    }
+
+    private void onCaptureImageResult(Intent data){
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        if (thumbnail != null){
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        }
+        File destination = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis()+".jpg");
+        FileOutputStream fo;
+        try{
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ShareDialog(thumbnail);
+    }
+
+    private void ShareDialog(final Bitmap shareImg){
+        if (checkIfAlreadyLogin() == false){
+            Toast.makeText(this, "Log in first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(shareImg)
+                .build();
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+        ShareDialog.show(this, content);
+    }
+
+    //Get user profile information: id, name, email, birthday
+    private void getUserProfile(){
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback(){
+                    @Override
+                    public void onCompleted(final JSONObject object, GraphResponse response) {
+                        Log.d("abc", object.toString());
+                        try {
+                            id = object.getString("id");
+                            name = object.getString("name");
+                            if (object.has("email")) {
+                                email = object.getString("email");
+                            }
+                            if (object.has("birthday")){
+                                birthday = object.getString("birthday");
+                            }
+                            updateLayout();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        Bundle params = new Bundle();
+        params.putString("fields", "id,name,email,birthday");
+        request.setParameters(params);
+        request.executeAsync();
+    }
+
+    //Get user friend list on Facebook
+    private void getUserFriendList(){
+        GraphRequest request = GraphRequest.newMyFriendsRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONArrayCallback() {
+                    @Override
+                    public void onCompleted(JSONArray objects, GraphResponse response) {
+                        Log.d("fl", objects.toString());
+
+                    }
+                });
+        request.executeAsync();
+    }
+
+
+
+}
